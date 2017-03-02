95ddf95721da2e27225ba9620291585ca459d047
gderycstj <gabriodery@hotmail.com>
2017-03-01 21:48:11 -0500

    publish and subscribe fait a botch

---
+++ log430-lab1/src/main/java/Event/AtmBus.java
@@ -0,0 +1,18 @@
+package Event;
+
+import com.google.common.eventbus.EventBus;
+
+public class AtmBus
+{	
+	private static AtmBus instance = null;
+	public static EventBus atmBus = new EventBus();;
+	/** Constructeur privé */
+	private AtmBus()
+	{
+	}
+ 
+	/** Point d'accès pour l'instance unique du singleton */
+	public static AtmBus getInstance()
+	{	return instance;
+	}
+}
