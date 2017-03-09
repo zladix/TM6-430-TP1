5c611e3cc61611dfe5718376117e746a867913c7
gderycstj <gabriodery@hotmail.com>
2017-03-08 21:59:20 -0500

    fix problème log

--- log430-lab1/src/main/java/edu/gordon/atm/NetworkToBank.java
+++ log430-lab1/src/main/java/edu/gordon/atm/NetworkToBank.java
@@ -1,9 +1,22 @@
 package edu.gordon.atm;
 
+import edu.gordon.banking.Balances;
+import edu.gordon.banking.Message;
+import edu.gordon.banking.Status;
+
 public interface NetworkToBank {
 
 	 public void openConnection();
 	 
 	 public void closeConnection();
-	  
+	 
+
+	    /** Send a message to bank
+	     *
+	     *  @param message the message to send
+	     *  @param balances (out) balances in customer's account as reported
+	     *         by bank
+	     *  @return status code returned by bank
+	     */
+	  public Status sendMessage(Message message, Balances balances);
 }
